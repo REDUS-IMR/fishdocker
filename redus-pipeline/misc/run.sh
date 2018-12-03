fb24d3d3af0b44de6798ace943b4076a3df85210
Ibrahim Umar <ibrahim.umar@uit.no>
2018-12-03 22:51:32 +0100

    redus-pipeline: Update Dockerfile

--- redus-pipeline/misc/run.sh
+++ redus-pipeline/misc/run.sh
@@ -17,13 +17,20 @@ if [ $status -ne 0 ]; then
 fi
 
 # Start ttyd
-ttyd -p 8002 sh &
+ttyd -r 3600 -p 8002 screen -r primary &
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start ttyd: $status"
   exit $status
 fi
 
+# Run the screen
+screen -dmS primary
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start screen: $status"
+  exit $status
+fi
 
 # Naive check runs checks once a minute to see if either of the processes exited.
 
@@ -34,9 +41,11 @@ while sleep 60; do
   PROCESS_2_STATUS=$?
   ps aux |grep ttyd |grep -q -v grep
   PROCESS_3_STATUS=$?
+  ps aux |grep screen |grep -q -v grep
+  PROCESS_4_STATUS=$?
   # If the greps above find anything, they exit with 0 status
   # If they are not all 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 -o $PROCESS_3_STATUS -ne 0]; then
+  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 -o $PROCESS_3_STATUS -ne 0 -o $PROCESS_4_STATUS -ne 0 ]; then
     echo "One of the processes has already exited."
     exit 1
   fi
